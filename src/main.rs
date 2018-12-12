6e01038e2cb623876baa87af7b636f1bf55f30f2
katanax <masaito1128@gmail.com>
2018-12-12 17:24:52 +0900

    commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+#![allow(unused_variables)]
 fn main() {
-    println!("Hello, world!");
+    let mut s1 = String::from("STRING 01");
+
+    {
+        // test 1:	
+        let s2 = &s1;
+        let s3 = &s1;
+        //let s4 = &mut s1;  // <- this will be error
+    }
+
+    {
+        // test 2:	
+        let s4 = &mut s1;
+        //let s3 = &s1;  // <- this will be error
+    }
 }
